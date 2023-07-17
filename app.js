d3067a848e987e4be27a7eea631467e449dd91ac
Raghu_Vardhan <your_email_address@example.com>
2023-07-17 10:39:41 +0530

    update PORT variable

--- app.js
+++ app.js
@@ -180,6 +180,6 @@ app.get("/logout", (req, res) =>{
     }); 
 })
 
-app.listen(3000, function(){
-    console.log("Server running at port 3000");
+app.listen(process.env.PORT, function(){
+    console.log("Server running at port ...");
 })
